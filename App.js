e6726d705ea204034fbeb1caeb79eb1fe17a5559
megadogalo <mylene.jpeg@gmail.com>
2024-05-23 20:24:21 -0300

    Test commit

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+<Text style={styles.titulo}> Mylene Cavalcanti </Text>
+<StatusBar style="auto" />
+
+
+       <Image
+        source={require('./assets/mylene.jpeg')}
+        style={styles.imagem}
+      />
+    
+
+      <TouchableOpacity onPress={() => console.log('Botão pressionado')}>
+      <Text style={styles.bio}>Sobre mim</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => console.log('Botão pressionado')}>
+      <Text style={styles.bio}>Sobre mim</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => console.log('Botão pressionado')}>
+      <Text style={styles.bio}>Sobre mim</Text>
+      </TouchableOpacity>
+
+      
+    
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0a0014',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  imagem: {
+  width: 200,
+  height: 200,
+  borderRadius:40,
+  marginBottom:20
+  },
+
+  
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#a34a0f'
+  },
+
+  bio: {
+    marginTop: 15,
+    color: '#a34a0f',
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+
 });
+
